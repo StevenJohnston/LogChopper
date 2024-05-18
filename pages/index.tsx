2ab9a7753a07cde6d9449738b35deed3541eb470
Steven Johnston <sjohnston@atomic.vc>
2024-05-18 10:52:12 -0400

    PWA for github pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,14 @@
 import Sidebar from "../src/_components/Sidebar";
 import ModuleDisplay from "../src/_components/ModuleDisplay";
-
-// import type { Metadata } from "next";
-
-
-// export const metadata: Metadata = {
-//   title: "LogChopper",
-//   description: "LogChopper",
-//   generator: "Next.js",
-//   manifest: "/manifest.json",
-//   keywords: ["LogChopper"],
-//   themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
-//   authors: [
-//     { name: "Steven Johnston" },
-//   ],
-//   viewport:
-//     "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
-//   icons: [
-//     { rel: "icon", url: "icon.png" },
-//   ],
-// };
+import Head from "next/head";
 
 export default function RootLayout() {
   return (
     <div className="flex flex-row h-full">
+      <Head>
+        <title>Log Chopper</title>
+
+      </Head>
       <Sidebar
         className="bg-slate-300 max-h-full"
       />
